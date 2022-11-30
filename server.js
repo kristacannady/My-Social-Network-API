c4150387f299e295adee9b008185477d51ef1093
Krista Cannady <kristalcannady@gmail.com>
2022-11-30 16:29:00 -0500

    change server

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.use(require("./routes"));
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-Network',
   {
@@ -20,7 +22,5 @@ mongoose.connect(
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
-
-app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
